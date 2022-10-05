30f563573641821f7fc283f29d4e99ea868fb7a3
angrajlatake <angraj.latake@gmail.com>
2022-10-05 19:08:01 -0400

    updated multer

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ dotenv.config();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/images/users");
+    cb(null, "/public/images/users");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
