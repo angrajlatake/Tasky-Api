db2ce45ce4db77673ff8cbfc6860fd94d96a35dd
angrajlatake <angraj.latake@gmail.com>
2022-10-05 19:37:32 -0400

    updated multer storage path

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ dotenv.config();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/images/users");
+    cb(null, "/public/images/users");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
